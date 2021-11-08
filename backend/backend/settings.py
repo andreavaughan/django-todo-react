d43587c63e0938873aee67fa41580b0220b32313
andreavaughan <amvaughan16@gmail.com>
2021-11-08 17:05:32 -0500

    created basic api: serializer, urls and view

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
     'todo',
 ]
 
@@ -53,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -133,3 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 env = environ.Env()
 
 environ.Env.read_env()
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
+]
