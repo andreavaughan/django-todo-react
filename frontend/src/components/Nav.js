4a794958693895ad1c03a4e716b8020ca6e41cfe
andreavaughan <amvaughan16@gmail.com>
2022-01-14 21:15:41 -0500

    slow navbar updates

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/nav.css'
 
 export const Nav = () => {
     return (
-        <nav>
+        <nav className="navbar">
             <ul>
                 <li>
                     <Link to="/">Home/Logo</Link>
